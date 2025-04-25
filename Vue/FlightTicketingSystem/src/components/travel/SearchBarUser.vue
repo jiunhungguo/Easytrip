c0beb5a0427ec7a39b9416bae038445295f2eccd
Juhok <viktorskuo@gmail.com>
2025-04-25 16:22:32 +0800

    calendar update

--- Vue/FlightTicketingSystem/src/components/travel/SearchBarUser.vue
+++ Vue/FlightTicketingSystem/src/components/travel/SearchBarUser.vue
@@ -9,7 +9,7 @@
         :class="[
           baseBtnClass,
           activeMode === mode.value
-            ? 'bg-blue-500 text-white border-blue-500'
+            ? 'bg-[#e8f0fe] text-[#1967d2] border-[#d2e3fc]'
             : 'bg-white text-gray-700 border-gray-300 hover:bg-gray-100 active:bg-gray-200',
         ]"
       >
